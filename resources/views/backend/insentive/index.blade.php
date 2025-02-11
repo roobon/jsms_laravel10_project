af8d6dc83fa3e24f2e58120d0e3ea1ac267e888d
Syed Ziaul Habib <roobon@gmail.com>
2025-02-12 00:09:41 +0600

    Insentive CRUD updates

--- resources/views/backend/insentive/index.blade.php
+++ resources/views/backend/insentive/index.blade.php
@@ -1,99 +1,107 @@
 @extends('backend.layouts.app')
 
 @section('styles')
-	@parent
+    @parent
 @endsection
 
 @section('title')
-	Investment List
+    Insentive List
 @endsection
 
 @section('content')
-<div class="container-fluid">
-				
-				<!-- Title -->
-				<div class="row heading-bg bg-green">
-					<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-					 
-					<h5 class="txt-light">Investment List</h5>
-					</div>
-					<!-- Breadcrumb -->
-					<div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-					  <ol class="breadcrumb">
-						<li><a href="index.html">Dashboard</a></li>
-						<li><a href="#"><span>Investments</span></a></li>
-						<li class="active"><span>Investment list</span></li>
-					  </ol>
-					</div>
-					<!-- /Breadcrumb -->
-				</div>
-				<!-- /Title -->
-				
-				<!-- Row -->
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="panel panel-default card-view">
-							<div class="panel-heading">
-								<div>
-									@include('backend.layouts.success')
-									{{-- <h6 class="panel-title txt-dark">Point List</h6> --}}
-								</div>
-								<div class="pull-right"><a href="{{route('investment.create')}}" class="btn btn-success">New Investment</a></div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="panel-wrapper collapse in">
-								<div class="panel-body">
-									<div class="table-wrap">
-										<div class="table-responsive">
-											<table id="example" class="table table-hover display  pb-30" >
-												<thead>
-													<tr>
-                                                        <th>ID</th>
-														<th>Investment Amount</th>
-														<th>Date</th>
-														<th>Business Name</th>
-														<th style="width: 10%;" class="text-center">Action</th>
-													</tr>
-												</thead>
-												<tfoot>
-													<tr>
-														<th>ID</th>
-														<th>Investment Amount</th>
-														<th>Date</th>
-														<th>Business Name</th>
-														<th style="width: 10%;" class="text-center">Action</th>
-													</tr>
-												</tfoot>
-												<tbody>
-													@foreach($items as $item)
-                                                    <tr>
-														<td>{{$loop->iteration}}</td>
-														<td>{{$item->investment_amount}}</td>
-														<td>{{$item->investment_date}}</td>
-														<td>{{$item->business->business_name}}</td>
-														<td style="width: 15%;" class="text-center">
-														<form onSubmit="return confirm('Are you sure to Delete')" action="{{route('investment.destroy', $item->id)}}" method="post">
-														<a class="btn btn-default btn-icon-anim btn-circle" href="{{route('investment.show', $item->id)}}"><i class="glyphicon glyphicon-search"></i></a>	
-														<a href="{{route('investment.edit', $item->id)}}" class="btn btn-primary btn-icon-anim btn-circle"><i class="glyphicon glyphicon-edit"></i></a>
-														@csrf
-														@method('DELETE')
-														<button class="btn btn-danger btn-icon-anim btn-circle"  type="submit" name="submit"><i class="glyphicon glyphicon-trash"></i></button>
-														</form>
-														</td>	
-													</tr>
-													@endforeach
-												</tbody>
-											</table>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>	
-					</div>
-				</div>
-				<!-- /Row -->
-			</div>
+    <div class="container-fluid">
 
+        <!-- Title -->
+        <div class="row heading-bg bg-green">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+
+                <h5 class="txt-light">Insentive List</h5>
+            </div>
+            <!-- Breadcrumb -->
+            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Dashboard</a></li>
+                    <li><a href="#"><span>Insentives</span></a></li>
+                    <li class="active"><span>Insentive list</span></li>
+                </ol>
+            </div>
+            <!-- /Breadcrumb -->
+        </div>
+        <!-- /Title -->
+
+        <!-- Row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default card-view">
+                    <div class="panel-heading">
+                        <div>
+                            @include('backend.layouts.success')
+                            {{-- <h6 class="panel-title txt-dark">Point List</h6> --}}
+                        </div>
+                        <div class="pull-right"><a href="{{ route('insentive.create') }}" class="btn btn-success">New
+                                Insentive</a></div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body">
+                            <div class="table-wrap">
+                                <div class="table-responsive">
+                                    <table id="example" class="table table-hover display  pb-30">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Insentive Amount</th>
+                                                <th>Date</th>
+                                                <th>Business Name</th>
+                                                <th style="width: 10%;" class="text-center">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Insentive Amount</th>
+                                                <th>Date</th>
+                                                <th>Business Name</th>
+                                                <th style="width: 10%;" class="text-center">Action</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            @foreach ($items as $item)
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $item->insentive_amount }}</td>
+                                                    <td>{{ $item->insentive_date }}</td>
+                                                    <td>{{ $item->business->business_name }}</td>
+                                                    <td style="width: 15%;" class="text-center">
+                                                        <form onSubmit="return confirm('Are you sure to Delete')"
+                                                            action="{{ route('investment.destroy', $item->id) }}"
+                                                            method="post">
+                                                            <a class="btn btn-default btn-icon-anim btn-circle"
+                                                                href="{{ route('investment.show', $item->id) }}"><i
+                                                                    class="glyphicon glyphicon-search"></i></a>
+                                                            <a href="{{ route('investment.edit', $item->id) }}"
+                                                                class="btn btn-primary btn-icon-anim btn-circle"><i
+                                                                    class="glyphicon glyphicon-edit"></i></a>
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button class="btn btn-danger btn-icon-anim btn-circle"
+                                                                type="submit" name="submit"><i
+                                                                    class="glyphicon glyphicon-trash"></i></button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Row -->
+    </div>
 @endsection
 
 @section('scripts')
